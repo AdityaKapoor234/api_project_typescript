b27f5b3b245ce3e6cf7dcb744f8678de796ade9b
divyanshchahar <divyanshchahar@outlook.com>
2024-03-08 18:56:52 +0530

    integrated add products functionality

--- src/ui/pages/ProductsPage.tsx
+++ src/ui/pages/ProductsPage.tsx
@@ -1,7 +1,23 @@
 import ProductInformationLayout from '../layouts/ProductInformationLayout';
 import useProductsApi from '../../hooks/useProductsApi';
+import AddProductFormLayout from '../layouts/AddProductFormLayout';
+import { useState } from 'react';
+import type { ProductInputTypes } from '../layouts/AddProductFormLayout';
+
+import { useAddProducts } from '../../hooks/useProductsApi';
 
 function ProductsPage() {
+	const [newProduct, setNewProduct] = useState({
+		productName: '',
+		productPrice: 0,
+	});
+
+	const updateNewProduct = (data: ProductInputTypes) => {
+		setNewProduct({ ...data });
+	};
+
+	const { mutate } = useAddProducts();
+
 	const { isLoading, error, data } = useProductsApi();
 
 	if (isLoading) return <h1 className="h1">Loading ...</h1>;
@@ -11,8 +27,14 @@ function ProductsPage() {
 	return (
 		<>
 			<div className="m-5">
-				<h1>Products Information</h1>
+				<h1 className="h1">Products Information</h1>
 				<ProductInformationLayout products={data.products} />
+				<h1 className="h1">Add new products</h1>
+				<AddProductFormLayout
+					handleSubmitFunc={mutate}
+					stateValue={newProduct}
+					stateFunc={updateNewProduct}
+				/>
 			</div>
 		</>
 	);
