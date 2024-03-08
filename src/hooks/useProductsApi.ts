555ad393cf585833806b22f444a6c58f597e76be
divyanshchahar <divyanshchahar@outlook.com>
2024-03-08 18:55:04 +0530

    added mutation functionality

--- src/hooks/useProductsApi.ts
+++ src/hooks/useProductsApi.ts
@@ -1,7 +1,20 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { ProductInputTypes } from '../ui/layouts/AddProductFormLayout';
 
 const fetchProducts = () =>
 	fetch('https://dummyjson.com/products').then((res) => res.json());
+
+const addProductsApi = (product: ProductInputTypes) =>
+	fetch('https://dummyjson.com/products/add', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			title: product.productName,
+			price: product.productPrice,
+		}),
+	}).then((res) => res.json());
 
 const useProductsApi = () => {
 	return useQuery({
@@ -10,4 +23,21 @@ const useProductsApi = () => {
 	});
 };
 
+const useAddProducts = () => {
+	const queryClient = useQueryClient();
+
+	return useMutation({
+		mutationFn: addProductsApi,
+		onSuccess: (data) => {
+			queryClient.setQueryData(['products'], (oldQueryData) => {
+				return {
+					...oldQueryData,
+					products: [...oldQueryData.products, data],
+				};
+			});
+		},
+	});
+};
+
 export default useProductsApi;
+export { useAddProducts };
